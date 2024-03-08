990e8b4919c226b0c2e4f6ef2c4cdef43d75a06b
Valentin Roussel <valentin.rssl@gmail.com>
2024-03-08 10:46:28 +0100

    i3: improve vim_scratch.sh

--- .i3/vim_scratch.sh
+++ .i3/vim_scratch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 tmp_file=$(mktemp)
-xfce4-terminal --disable-server -T "__vim_scratch" -e "nvim -c startinsert ${tmp_file}" && xclip -selection clipboard < $tmp_file
+xclip -o > ${tmp_file}
+xfce4-terminal --disable-server -T "__vim_scratch" -e "nvim -c startinsert ${tmp_file}"
+xclip -selection clipboard < $tmp_file
