ff66ee1cbaad6e533fb5098e357f52cb9d0b81d0
Valentin Roussel <valentin.rssl@gmail.com>
2024-03-08 10:46:26 +0100

    no 'spelling check' for vim_scratch becaise it's annoying

--- .i3/vim_scratch.sh
+++ .i3/vim_scratch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tmp_file=$(mktemp)
-xfce4-terminal --disable-server -T "__vim_scratch" -e "nvim -c startinsert -c 'setlocal spell' ${tmp_file}" && xclip -selection clipboard < $tmp_file
+xfce4-terminal --disable-server -T "__vim_scratch" -e "nvim -c startinsert ${tmp_file}" && xclip -selection clipboard < $tmp_file
