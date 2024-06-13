3b80f83517e54534e9d2466acf2228a9903c38af
vRoussel <valentin.rssl@gmail.com>
2024-06-13 14:18:45 +0200

    add secondary bind for vim_scratch with C-c clipboard

--- .i3/vim_scratch.sh
+++ .i3/vim_scratch.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    exit 1
+fi
+
 tmp_file=$(mktemp)
-xclip -o > ${tmp_file}
+
+if [ "$1" = "clipboard" ]; then
+    xclip -selection clipboard -o > ${tmp_file}
+elif [ "$1" = "primary" ]; then
+    xclip -selection primary -o > ${tmp_file}
+elif [ "$1" = "stdin" ]; then
+    cat > ${tmp_file}
+elif [ "$1" = "arg" ]; then
+    echo -n "$2" > ${tmp_file}
+fi
+
 xfce4-terminal --disable-server -T "__vim_scratch" -e "nvim -c startinsert ${tmp_file}"
 xclip -selection clipboard < $tmp_file
