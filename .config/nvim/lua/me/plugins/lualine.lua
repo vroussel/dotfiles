a5798f7ae19f95e2a87c06838adb12e65927bb0d
vRoussel <valentin.rssl@gmail.com>
2024-06-13 14:49:31 +0200

    nvim: add arrow.nvim indicator in lualine

--- .config/nvim/lua/me/plugins/lualine.lua
+++ .config/nvim/lua/me/plugins/lualine.lua
@@ -2,15 +2,23 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+		"otavioschwanck/arrow.nvim",
 	},
 	config = function()
-		local lualine = require("lualine")
 		local lazy_status = require("lazy.status")
+		local statusline = require("arrow.statusline")
+
 		require("lualine").setup({
 			options = {
 				theme = "tokyonight",
 			},
 			sections = {
+				lualine_c = {
+					function()
+						return statusline.text_for_statusline_with_icons(nil)
+					end,
+					"filename",
+				},
 				lualine_x = {
 					{
 						lazy_status.updates,
