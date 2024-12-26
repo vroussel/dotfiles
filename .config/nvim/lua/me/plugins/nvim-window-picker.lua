a62c0622436efc1fa13faf2f43c4d71d30aa3d23
vRoussel <valentin.rssl@gmail.com>
2024-12-26 17:22:08 +0100

    nvim: replace qfenter plugin with custom lua code

--- .config/nvim/lua/me/plugins/nvim-window-picker.lua
+++ .config/nvim/lua/me/plugins/nvim-window-picker.lua
@@ -4,7 +4,9 @@ return {
     event = "VeryLazy",
     version = "2.*",
     config = function()
-        require("window-picker").setup({
+        local window_picker = require("window-picker")
+
+        window_picker.setup({
             hint = "floating-big-letter",
             filter_rules = {
                 autoselect_one = true,
@@ -15,6 +17,36 @@ return {
                     buftype = { "prompt" },
                 },
             },
+        })
+
+        ---@param mode "vsplit"|"split"|nil
+        local function open_qf_item(mode)
+            local qf_id = vim.fn.win_getid()
+            local picked_id = window_picker.pick_window({ filter_rules = { include_current_win = false } })
+            if picked_id then
+                vim.fn.win_gotoid(picked_id)
+                if mode then
+                    vim.cmd(mode)
+                end
+                vim.fn.win_gotoid(qf_id)
+                local cr = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+                vim.api.nvim_feedkeys(cr, "n", false)
+            end
+        end
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "qf",
+            callback = function()
+                vim.keymap.set("n", "<c-v>", function()
+                    open_qf_item("vsplit")
+                end)
+                vim.keymap.set("n", "<c-x>", function()
+                    open_qf_item("split")
+                end)
+                vim.keymap.set("n", "<cr>", function()
+                    open_qf_item(nil)
+                end)
+            end,
         })
     end,
 }
