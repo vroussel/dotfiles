a989ca422ea6714a5aa95ea5d868428051d92e55
vRoussel <valentin.rssl@gmail.com>
2024-12-27 11:40:12 +0100

    nvim: update statuscol conf

--- .config/nvim/lua/me/plugins/statuscol.lua
+++ .config/nvim/lua/me/plugins/statuscol.lua
@@ -1,13 +1,27 @@
 return {
     "luukvbaal/statuscol.nvim",
+    enabled = true,
     config = function()
         local statuscol = require("statuscol")
         local builtin = require("statuscol.builtin")
         statuscol.setup({
             relculright = true,
             segments = {
-                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-                { text = { "%s" }, click = "v:lua.ScSa" },
+                { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+                {
+                    sign = { namespace = { "diagnostic/signs" }, colwidth = 2, maxwidth = 1, auto = true },
+                    click = "v:lua.ScSa",
+                },
+                {
+                    sign = {
+                        name = { ".*" },
+                        namespace = { ".*" },
+                        maxwidth = 1,
+                        colwidth = 2,
+                        auto = true,
+                    },
+                    click = "v:lua.ScSa",
+                },
                 { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
             },
         })
