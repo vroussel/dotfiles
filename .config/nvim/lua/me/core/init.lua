2a9f403b5ae41ad48d1cfd0662a42dfa3d59150e
vRoussel <valentin.rssl@gmail.com>
2025-02-27 08:17:57 +0100

    nvim: auto resize windows when terminal gets resized

--- .config/nvim/lua/me/core/init.lua
+++ .config/nvim/lua/me/core/init.lua
@@ -3,3 +3,8 @@ require("me.core.options")
 require("me.core.trailing_spaces")
 require("me.core.lazygit")
 require("me.core.title")
+
+-- auto resize windows when terminal gets resized
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
