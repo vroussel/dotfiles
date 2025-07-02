546e4ca54b7705f1237e28bbd22f2345c80e8efe
Valentin Roussel <valentin.rssl@gmail.com>
2025-07-02 17:42:19 +0200

    nvim: disable automatic_enable for mason_lspconfig, it causes duplicate LS

--- .config/nvim/lua/me/plugins/lspconfig.lua
+++ .config/nvim/lua/me/plugins/lspconfig.lua
@@ -16,6 +16,7 @@ return {
         local mason_lspconfig = require("mason-lspconfig")
         mason_lspconfig.setup({
             automatic_installation = true,
+            automatic_enable = false,
             -- automatic_installation = {
             --     exclude = {
             --         "pylsp",
