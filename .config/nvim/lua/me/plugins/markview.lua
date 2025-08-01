01c192acb3e0112bbb29d757ac66648179096cc0
vRoussel <valentin.rssl@gmail.com>
2025-08-01 16:21:11 +0200

    use nvim-treesitter main branch (master is not maintained anymore)

--- .config/nvim/lua/me/plugins/markview.lua
+++ .config/nvim/lua/me/plugins/markview.lua
@@ -4,11 +4,6 @@ return {
     -- ft = "markdown" -- If you decide to lazy-load anyway
 
     dependencies = {
-        -- You will not need this if you installed the
-        -- parsers manually
-        -- Or if the parsers are in your $RUNTIMEPATH
-        "nvim-treesitter/nvim-treesitter",
-
         "nvim-tree/nvim-web-devicons",
     },
 }
