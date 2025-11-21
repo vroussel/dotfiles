ab1e8369bb14f843b96256c76ad38d84344abf7b
vRoussel <valentin.rssl@gmail.com>
2025-11-21 16:58:05 +0100

    i3: ask for vim_scratch source with rofi on super+shift+g

--- bin/vim_scratch.sh
+++ bin/vim_scratch.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] && [ $# -ne 2 ]; then
-    exit 1
+menu () {
+    title="$1"
+    shift
+    rofi \
+        -dmenu
+        "$@"
+}
+
+trim() {
+    awk '{$1=$1};1' || exit 0
+}
+
+if [ $# -eq 1 ]; then
+    source="$1"
+else
+    source=$(menu 'vim scratch source' << EOF | trim
+        clipboard
+        selection
+        stdin
+        blank
+EOF
+) || exit 0
 fi
 
 tmp_file=$(mktemp)
 
-if [ "$1" = "clipboard" ]; then
-    xclip -selection clipboard -o > ${tmp_file}
-elif [ "$1" = "primary" ]; then
-    xclip -selection primary -o > ${tmp_file}
-elif [ "$1" = "stdin" ]; then
-    cat > ${tmp_file}
-elif [ "$1" = "arg" ]; then
-    echo -n "$2" > ${tmp_file}
+if [ "$source" = "clipboard" ]; then
+    xclip -selection clipboard -o > "${tmp_file}"
+elif [ "$source" = "selection" ]; then
+    xclip -selection primary -o > "${tmp_file}"
+elif [ "$source" = "stdin" ]; then
+    cat > "${tmp_file}"
 fi
 
-alacritty -o ipc_socket=false -T "__vim_scratch" -e nvim -c 'set nofixeol' ${tmp_file}
-xclip -selection clipboard < $tmp_file
+alacritty -o ipc_socket=false -T "__vim_scratch" -e nvim -c 'set nofixeol' "${tmp_file}"
+xclip -selection clipboard < "$tmp_file"
