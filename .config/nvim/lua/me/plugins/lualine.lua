8b38e945d463b054796c36c356305fc6211c94c3
vRoussel <git@vrsl.fr>
2026-03-20 18:00:36 +0100

    nvim: remote full path from status line

--- .config/nvim/lua/me/plugins/lualine.lua
+++ .config/nvim/lua/me/plugins/lualine.lua
@@ -1,30 +1,27 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-        local lualine = require("lualine")
-        local lazy_status = require("lazy.status")
-        require("lualine").setup({
-            options = {
-                theme = 'tokyonight',
-            },
-            sections = {
-                lualine_c = {
-                    { 'filename', path = 3 }
-                },
-                lualine_x = {
-                    {
-                        lazy_status.updates,
-                        cond = lazy_status.has_updates,
-                        color = { fg = "#ff9e64" },
-                    },
-                    { "encoding", },
-                    { "fileformat" },
-                    { "filetype" },
-                }
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status")
+		require("lualine").setup({
+			options = {
+				theme = "tokyonight",
+			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
