0320238f6ae1feb7eb98bb1dd576faf3d57cdd68
vRoussel <git@vrsl.fr>
2026-03-20 18:00:46 +0100

    nvim: remove manual cursorline highlight in yellow, it works fine by default

--- .config/nvim/lua/me/plugins/colorscheme.lua
+++ .config/nvim/lua/me/plugins/colorscheme.lua
@@ -5,11 +5,6 @@ return {
         local tokyonight = require("tokyonight")
         tokyonight.setup({
             style = "storm",
-            on_highlights = function(highlights, colors)
-                highlights.CursorLineNr = {
-                    fg = colors.yellow,
-                }
-            end,
         })
         vim.cmd([[colorscheme tokyonight]])
     end,
