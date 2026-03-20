0a09ceca8610cca3e0d1b314dfb5d6f3ead9191f
vRoussel <git@vrsl.fr>
2026-03-20 18:00:41 +0100

    nvim: improve quick dotfiles edit keymaps

--- .config/nvim/lua/me/core/keymaps/dotfiles.lua
+++ .config/nvim/lua/me/core/keymaps/dotfiles.lua
@@ -1,11 +1,24 @@
 vim.keymap.set("n", "<leader>cn", function()
 	vim.cmd("tabnew ~/.config/nvim/")
+	vim.cmd("tcd ~/.config/nvim")
 end, { desc = "neovim conf" })
 
 vim.keymap.set("n", "<leader>ci", function()
 	vim.cmd("tabnew ~/.config/i3/config")
+	vim.cmd("tcd ~/.config/i3")
 end, { desc = "i3 conf" })
 
 vim.keymap.set("n", "<leader>cb", function()
 	vim.cmd("tabnew ~/.bashrc")
+	vim.cmd("tcd ~")
+end, { desc = "bashrc conf" })
+
+vim.keymap.set("n", "<leader>cl", function()
+	vim.cmd("tabnew ~/.config/alacritty/alacritty.toml")
+	vim.cmd("tcd ~/.config/alacritty")
+end, { desc = "bashrc conf" })
+
+vim.keymap.set("n", "<leader>cf", function()
+	vim.cmd("tabnew ~/.config/fish/config.fish")
+	vim.cmd("tcd ~/.config/fish")
 end, { desc = "bashrc conf" })
