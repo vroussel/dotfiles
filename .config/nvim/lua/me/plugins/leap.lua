acf073581a0e37480a061ae52c1d4ffbfbfa2bbd
vRoussel <git@vrsl.fr>
2026-03-20 18:00:45 +0100

    nvim: change lazy/mason keymaps to make room for leap

--- .config/nvim/lua/me/plugins/leap.lua
+++ .config/nvim/lua/me/plugins/leap.lua
@@ -2,5 +2,6 @@ return {
     "ggandor/leap.nvim",
     config = function()
         vim.keymap.set("n", "<leader>l", "<Plug>(leap)")
+        vim.keymap.set("n", "<leader>L", "<Plug>(leap-anywhere)")
     end,
 }
