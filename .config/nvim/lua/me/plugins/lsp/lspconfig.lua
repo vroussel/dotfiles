8bfdd6e04b262b006f0cf146792964eb55a81c23
vRoussel <git@vrsl.fr>
2026-03-20 18:00:40 +0100

    do not use telescope for references

--- .config/nvim/lua/me/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/me/plugins/lsp/lspconfig.lua
@@ -34,7 +34,7 @@ return {
 
 			-- set keybinds
 			opts.desc = "Show LSP references"
-			keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+			keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
 			opts.desc = "Go to declaration"
 			keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
