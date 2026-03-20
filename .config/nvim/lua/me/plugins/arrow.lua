2b9b29da69e87d7c25b2647243247eb3f863461a
vRoussel <git@vrsl.fr>
2026-03-20 18:00:38 +0100

    nvim: add plugins (arrow/blame/hardtime)

---
+++ .config/nvim/lua/me/plugins/arrow.lua
@@ -0,0 +1,19 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	opts = {
+		show_icons = true,
+		leader_key = "<leader>;", -- Recommended to be a single key
+		mappings = {
+			edit = "e",
+			delete_mode = "d",
+			clear_all_items = "C",
+			toggle = "s", -- used as save if separate_save_and_remove is true
+			open_vertical = "<c-v>",
+			open_horizontal = "<c-x>",
+			quit = "q",
+			remove = "x", -- only used if separate_save_and_remove is true
+			next_item = "]",
+			prev_item = "[",
+		},
+	},
+}
---
+++ .config/nvim/lua/me/plugins/hardtime.lua
@@ -0,0 +1,19 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		disabled_filetypes = {
+			"qf",
+			"netrw",
+			"NvimTree",
+			"lazy",
+			"mason",
+			"neo-tree",
+			"neo-tree-popup",
+			"NeogitStatus",
+			"NeogitCommitSelectView",
+			"NeogitRebaseTodo",
+			"DressingInput",
+		},
+	},
+}
