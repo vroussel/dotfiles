bb6b5993eea19f6871be658f75b47b514e5e3881
vRoussel <git@vrsl.fr>
2026-03-20 18:00:41 +0100

    nvim: autoindent

--- .config/nvim/lua/me/plugins/colorscheme.lua
+++ .config/nvim/lua/me/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-        local tokyonight = require("tokyonight")
-        tokyonight.setup({
-            style = "storm",
-            on_highlights = function(highlights, colors)
-                highlights.CursorLineNr = {
-                    fg = colors.yellow
-                }
-            end
-        })
-        vim.cmd([[colorscheme tokyonight]])
-    end,
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		local tokyonight = require("tokyonight")
+		tokyonight.setup({
+			style = "storm",
+			on_highlights = function(highlights, colors)
+				highlights.CursorLineNr = {
+					fg = colors.yellow,
+				}
+			end,
+		})
+		vim.cmd([[colorscheme tokyonight]])
+	end,
 }
