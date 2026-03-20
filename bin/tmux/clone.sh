f5c9cfb89e34a12605f0e456ec4f07da68d6fdb2
vRoussel <git@vrsl.fr>
2026-03-20 18:00:48 +0100

    tmux: keep sudo_user and cwd when cloning pane

--- bin/tmux/clone.sh
+++ bin/tmux/clone.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 set -e
 
+get_extra_env() {
+    cmd="$1"
+    ret=""
+    PROMPT_PATTERN=" " #\u2005, special space
+
+    x=$(tmux capture-pane -p -S '-' -J)
+    last_prompt=$(echo "$x" | tac | grep --max-count 1 "$PROMPT_PATTERN")
+    user=$(echo "$last_prompt" | cut -d@ -f 1)
+    path=$(echo "$last_prompt" | grep -Po "(?<=:).*(?=$PROMPT_PATTERN)")
+
+    ret+="SSHS_CWD=$path"
+    if ! [[ "$cmd" =~ (-l\ $user)|($user@) ]]; then
+        ret+=" SSHS_SUDO_USER=$user"
+    fi
+    echo "$ret"
+}
+
+reset_extra_env() {
+    echo "-u SSHS_CWD -u SSHS_SUDO_USER"
+}
+
 pid="$(tmux display-message -p '#{pane_pid}')"
 pid="$(ps -o tpgid:1= -p "$pid")"
 dir="$(readlink -f "/proc/$pid/cwd")"
-cd -- "${dir:?}"
-exe="$(readlink -f "/proc/$pid/exe")"
-readarray -d '' args <"/proc/$pid/cmdline"
-name="${args[0]}"
-args=("${args[@]:1}")
+readarray -d '' cmd <"/proc/$pid/cmdline"
+name="${cmd[0]}"
+args=("${cmd[@]:1}")
 
-if [ "$name" == "/bin/ssh" ]; then
-    tmux "$@" -c "$dir" bash -c "${exe@Q} ${args[*]@Q}; bash"
-elif [[ "$name" == "bash" && "${args[0]}" == "-c" && "${args[1]}" =~ ^\'?/usr/bin/ssh.* ]]; then
-    tmux "$@" -c "$dir" bash -c "${args[1]}"
+if [[ "$name" == "/bin/bash" && "${args[0]}" == /home/valentin/bin/sshs ]]; then
+    extra_env=$(get_extra_env "${cmd[*]}")
+    no_extra_env=$(reset_extra_env)
+    tmux "$@" env $extra_env /bin/bash -c "${args[*]}; env $no_extra_env bash"
+elif [[ "$name" == "/bin/bash" && "${args[0]}" == "-c" && "${args[1]}" =~ ^/home/valentin/bin/sshs ]]; then
+    extra_env=$(get_extra_env "${cmd[*]}")
+    tmux "$@" env $extra_env /bin/bash -c "${args[*]:1}"
 else
-    tmux "$@" -c "$dir" bash
+    tmux "$@" -c "$dir" "${cmd[*]}"
 fi
