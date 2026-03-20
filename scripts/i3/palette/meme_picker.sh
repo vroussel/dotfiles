cccb874dfc10f8a8cc2b5d75798bb0906decdba7
vRoussel <git@vrsl.fr>
2026-03-20 18:00:51 +0100

    script_palette.sh: add special case for gifs

--- scripts/i3/palette/meme_picker.sh
+++ scripts/i3/palette/meme_picker.sh
@@ -14,7 +14,14 @@ menu() {
 
 meme_dir="$HOME/scripts/i3/palette/memes"
 
-meme=$(find "$meme_dir" -mindepth 1 | xargs basename | menu "Pick your meme") || exit 0
+meme=$(find "$meme_dir" -mindepth 1 | xargs -L 1 basename | menu "Pick your meme") || exit 0
 
-xclip -selection clipboard -t image/png "$meme_dir/$meme"
+extension="${meme##*.}"
+if [ "$extension" == "gif" ]; then
+    target="text/plain"
+else
+    target="image/$extension"
+fi
+
+xclip -selection clipboard -t "$target" "$meme_dir/$meme"
 notify-send -t 1000 Done
