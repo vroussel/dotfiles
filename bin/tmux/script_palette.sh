c635c3edbbc570e666f68445bd17f5c6ace9442b
vRoussel <git@vrsl.fr>
2026-03-20 18:00:49 +0100

    tmux: refactor "sessionizer" and "cmd palette"

---
+++ bin/tmux/script_palette.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+menu () {
+    title="$1"
+    shift
+    fzf \
+        -i \
+        --tmux \
+        --border --border-label "$title" \
+        --bind ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down \
+        "$@"
+}
+
+trim() {
+    awk '{$1=$1};1' || exit 0
+}
+
+SCRIPTS_PATH="$HOME/bin/tmux/scripts/"
+
+pick=$(find "$SCRIPTS_PATH" -mindepth 1 | xargs -L 1 basename | menu "Script palette") || exit 0
+"$SCRIPTS_PATH/$pick"
--- bin/tmux/script_picker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPTS_PATH="$HOME/bin/tmux/scripts/"
-
-pick=$(find "$SCRIPTS_PATH" -mindepth 1 | xargs -L 1 basename | fzf --tmux --border --border-label "Script picker") || exit 0
-"$SCRIPTS_PATH/$pick"
