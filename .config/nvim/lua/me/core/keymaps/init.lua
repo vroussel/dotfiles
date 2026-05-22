a364b25179a9cad2bc8edeefcbfa682f35e34354
vRoussel <git@vrsl.fr>
2026-05-22 15:56:06 +0200

    nvim: add quick restart keymap

--- .config/nvim/lua/me/core/keymaps/init.lua
+++ .config/nvim/lua/me/core/keymaps/init.lua
@@ -61,3 +61,10 @@ vim.keymap.set("n", "<c-q>", function()
         vim.cmd("copen")
     end
 end)
+
+-- Quick restart
+vim.keymap.set("n", "<leader>R", function()
+    local session = vim.fn.stdpath("state") .. "/restart_session.vim"
+    vim.cmd("mksession! " .. vim.fn.fnameescape(session))
+    vim.cmd("restart source " .. vim.fn.fnameescape(session))
+end, { desc = "Restart Neovim" })
