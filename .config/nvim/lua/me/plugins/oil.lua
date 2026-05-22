c6fb4fcad3eaa5046e3c3ff59367e34469596446
vRoussel <git@vrsl.fr>
2026-05-22 15:23:24 +0200

    nvim: do not override ctrl-v in visual mode in oil

--- .config/nvim/lua/me/plugins/oil.lua
+++ .config/nvim/lua/me/plugins/oil.lua
@@ -9,8 +9,8 @@ return {
     config = function()
         require("oil").setup({
             keymaps = {
-                ["<C-v>"] = { "actions.select", opts = { close = true, vertical = true } },
-                ["<C-x>"] = { "actions.select", opts = { close = true, horizontal = true } },
+                ["<C-v>"] = { "actions.select", opts = { close = true, vertical = true }, mode = "n" },
+                ["<C-x>"] = { "actions.select", opts = { close = true, horizontal = true }, mode = "n" },
             },
         })
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
