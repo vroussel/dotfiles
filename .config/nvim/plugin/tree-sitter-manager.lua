5924d72babcb576120c3fe1e30d6976a26cb83dc
vRoussel <git@vrsl.fr>
2026-09-18 12:09:48 +0200

    nvim: nvim-treesitter is back

--- .config/nvim/plugin/tree-sitter-manager.lua
+++ .config/nvim/plugin/tree-sitter-manager.lua
@@ -1,25 +1,41 @@
-vim.pack.add({ "https://github.com/romus204/tree-sitter-manager.nvim" })
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
 
-require("tree-sitter-manager").setup({
-    ensure_installed = {
-        "json",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "c",
-        "cpp",
-        "python",
-        "rust",
-        "vimdoc",
-        "regex",
-        "just",
-        "sql",
-    },
+require("nvim-treesitter").install({
+    "json",
+    "yaml",
+    "html",
+    "css",
+    "markdown",
+    "markdown_inline",
+    "bash",
+    "lua",
+    "vim",
+    "dockerfile",
+    "gitignore",
+    "c",
+    "cpp",
+    "python",
+    "rust",
+    "vimdoc",
+    "regex",
+    "just",
+    "sql",
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == "nvim-treesitter" and kind == "update" then
+            if not ev.data.active then
+                vim.cmd.packadd("nvim-treesitter")
+            end
+            vim.cmd("TSUpdate")
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+        pcall(vim.treesitter.start)
+    end,
 })
